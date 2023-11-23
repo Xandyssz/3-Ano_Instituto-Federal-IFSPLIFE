345e19befae23dced89ac24aa5d0c4ddc9e23a44
Xandyszz <xandyofcff@gmail.com>
2023-11-22 22:45:14 -0300

    Delete TESTE_DE_ERRO.java

--- ifsplife/src/ifsplife/model/TESTE_DE_ERRO.java
+++
@@ -1,9 +0,0 @@
-package ifsplife.model;
-
-import ifsplife.control.GerenciadorConexao;
-
-public class TESTE_DE_ERRO {
-    public static void main(String[] args) {
-        GerenciadorConexao.getGerente();
-    }
-}
